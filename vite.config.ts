a735a620d5b350ebc49afba4587c9c7326328872
Matteo <matnice23@gmail.com>
2025-03-18 12:41:34 +0100

    Added image optimizing plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+
+const isProduction = process.env.NODE_ENV === "production";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), isProduction && ViteImageOptimizer()].filter(Boolean),
   test: {
     globals: true,
     environment: "jsdom",
